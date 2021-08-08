61a931631804df50931d16ba121230c2697fad3f
kiran0916 <12438870+kiran0916@users.noreply.github.com>
2021-08-07 17:54:04 -0700

    inital check-in

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace solutions_leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
